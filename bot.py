d97e2bc566b435155b8b7264acaf53634bef2b96
MoHiT <145571381+mohit828@users.noreply.github.com>
2024-09-25 13:09:07 +0530

    Update bot.

--- bot.py
+++ bot.py
@@ -16,13 +16,16 @@ logging.getLogger("cinemagoer").setLevel(logging.ERROR)
 logger = logging.getLogger(__name__)
 
 class Bot(Client):
+
     def __init__(self):
         super().__init__(
             name="Professor-Bot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            plugins=dict(root="plugins")
+            workers=200,
+            plugins={"root": "plugins"},
+            sleep_threshold=10,
         )
 
     async def start(self):
