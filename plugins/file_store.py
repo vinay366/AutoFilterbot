397e1ce09b7ecd3e0b696bd4861ca955024b1a3e
Vinay Mudiraj <78520329+vinay366@users.noreply.github.com>
2025-03-10 18:45:46 +0530

    Update file_store.py

--- plugins/file_store.py
+++ plugins/file_store.py
@@ -35,10 +35,10 @@ async def gen_link_s(bot, message):
 @Client.on_message(filters.command(['batch', 'pbatch']) & filters.create(allowed))
 async def gen_link_batch(bot, message):
     if " " not in message.text:
-        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/Devilservers</code>.")
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/TCU_TCU</code>.")
     links = message.text.strip().split(" ")
     if len(links) != 3:
-        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/Devilservers</code>.")
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/TCU_TCU</code>.")
     cmd, first, last = links
     regex = re.compile("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
     match = regex.match(first)
